7bc7537c1bcecb8f85a493dd82a4efe9560742bf
Julia Jubat <juliamariajubatalvesdossantos@gmail.com>
2026-07-22 15:53:18 -0300

    adição de botão

---
+++ scripts/loading.js
@@ -0,0 +1,9 @@
+setTimeout(() => {
+
+    document.getElementById("loading").style.display = "none";
+
+}, 5000);   
+
+setTimeout(() => {
+    document.getElementById("btn").style.display = "block";
+}, 5000);
